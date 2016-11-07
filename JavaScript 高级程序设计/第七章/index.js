27ed4f4db21015c24df6d2049a0477a55e4b0545
tombsky <tombsky@163.com>
2016-11-07 23:53:35 +0800

    update

--- JavaScript 高级程序设计/第七章/index.js
+++ JavaScript 高级程序设计/第七章/index.js
@@ -17,5 +17,31 @@ function sayHi(){
 var functionNameExpress = function(arg0,arg1,arg2){
     //函数体
 }
+/***************************/
+/***
+ * 此处方法anotherFactorial方法体与原factorial方法体内容相同,但是方法体中引用了factorial,此时为null故会报错
+ * @param num
+ * @returns {number}
+ */
+function factorial(num){
+    if(num<=1){
+        return 1;
+    }else{
+        return num*fatorial(num-1);
+    }
+}
 
 
+var anotherFactorial = factorial;
+factorial=null;
+// console.log(anotherFactorial(4));
+/***************************/
+
+function factorial2(num){
+    if(num<=1){
+        return 1;
+    }else{
+        //arguments.callee()指向正在执行的函数指针
+        return num*arguments.callee(num-1);
+    }
+}
